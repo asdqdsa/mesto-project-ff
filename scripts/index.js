42c2561bea9759e5581c3f29c446fa5973409afc
asdqdsa <asdqdsa@protonmail.com>
2023-11-03 16:52:54 +0400

    fix: Create card, display cards logic

--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,19 @@ const cardTemplate = document.querySelector('#card-template');
 const cardContent = cardTemplate.content;
 
 // @todo: DOM узлы
-const addCardBtn = document.querySelector('.profile__add-button');
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(cardIndex = 0) {
-  const [cardName, cardLink] = [
-    initialCards[cardIndex].name,
-    initialCards[cardIndex].link,
-  ];
+function createCard(card, removeCardHandler) {
   const cardContentClone = cardContent.cloneNode(true);
-  cardContentClone.querySelector('.card__title').textContent = cardName;
-  cardContentClone.querySelector('.card__image').src = cardLink;
+  cardContentClone.querySelector('.card__title').textContent = card.name;
+  cardContentClone.querySelector('.card__image').src = card.link;
+  cardContentClone.querySelector(
+    '.card__image',
+  ).alt = `Фотография местности ${card.name}`;
   cardContentClone
     .querySelector('.card__delete-button')
-    .addEventListener('click', (event) => removeCard(event));
+    .addEventListener('click', (event) => removeCardHandler(event));
 
   return cardContentClone;
 }
@@ -29,20 +27,6 @@ function removeCard(event) {
 }
 
 // @todo: Вывести карточки на страницу
-function init(createCard) {
-  for (let i = 0; i < initialCards.length; i += 1) {
-    const card = createCard(i);
-    placesList.append(createCard(i));
-  }
-}
-
-// Добавление случайной карточки
-function addRandomCard(createCard) {
-  const random = Math.floor(Math.random() * initialCards.length);
-  const card = createCard(random);
-  placesList.append(card);
-}
-
-addCardBtn.addEventListener('click', () => addRandomCard(createCard));
-
-init(createCard);
+initialCards.forEach((card) => {
+  placesList.append(createCard(card, removeCard));
+});
